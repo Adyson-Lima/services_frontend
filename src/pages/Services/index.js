f720f8b3fc967ba753d56d8ac3e06b8ad18ad699
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-20 11:18:25 -0300

    3° commit - tdd tela Services: Existe card em Services? GREEN

---
+++ src/pages/Services/index.js
@@ -0,0 +1,18 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+// import api from '../../services/api';
+
+export default function Services(){
+
+  return(
+    <div data-testid="mycard" className="card border-primary" style={{marginTop: '20px'}} >
+      <div className="card-header bg-primary" style={{color: '#fff'}}>
+        Services Crud
+      </div>
+      <div className="card-body">
+
+      </div>
+    </div>
+  );
+
+}
