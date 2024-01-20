2cfd1b40854d7027dcf48c40be90eee3876067bb
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-20 11:26:36 -0300

    8° commit - tdd tela Services: Existe botão editar em Services? RED

--- src/tests/Services.test.js
+++ src/tests/Services.test.js
@@ -24,4 +24,8 @@ describe('Testes da tela Services', () => {
     expect(screen.getByTestId('mytable')).toBeInTheDocument();
   });
 
+  it('Existe botão editar em Services?', () => {
+    expect(screen.getByTestId('mybtn1')).toBeInTheDocument();
+  });
+
 });
