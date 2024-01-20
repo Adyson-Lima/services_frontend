ba3e44764d47e99f241c090c298df6adde2ff348
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-20 12:07:57 -0300

    24° commit - logica read implementada

--- src/pages/Services/index.js
+++ src/pages/Services/index.js
@@ -1,8 +1,17 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-// import api from '../../services/api';
+import api from '../../services/api';
 
 export default function Services(){
+
+  const[my_services, setServices] = useState([]);
+  const navigate = useNavigate();
+
+  // read, busca todos os registros na api
+  useEffect(() => {
+    api.get('api/v1/services',{})
+    .then(response => {setServices(response.data)})
+  }, []);
 
   return(
     <div data-testid="mycard" className="card border-primary" style={{marginTop: '20px'}} >
@@ -17,27 +26,30 @@ export default function Services(){
         <table data-testid="mytable" className="table table-hover">
           <thead>
             <tr>
-              <th scope="col">##</th>
-              <th scope="col">##</th>
-              <th scope="col">##</th>
-              <th scope="col">##</th>
+              <th scope="col">Id</th>
+              <th scope="col">Nome</th>
+              <th scope="col">Valor</th>
+              <th scope="col">Ações</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row"></th>
-                <td></td>
-                <td></td>
-                <td>
+            {my_services.map(service => (
+              <tr key={service.id}>
+                <th scope="row">{service.id}</th>
+                  <td>{service.name}</td>
+                  <td>{service.value}</td>
+                  <td>
 
-                  <button data-testid="mybtn1" type="button"
-                  className="btn btn-outline-info" style={{margin: '2px'}}>Editar</button>
+                    <button data-testid="mybtn1" type="button"
+                    className="btn btn-outline-info" style={{margin: '2px'}}>Editar</button>
 
-                  <button data-testid="mybtn2" type="button"
-                  className="btn btn-outline-danger" style={{margin: '2px'}}>Excluir</button>
+                    <button data-testid="mybtn2" type="button"
+                    className="btn btn-outline-danger" style={{margin: '2px'}}>Excluir</button>
 
-                </td>
-            </tr>
+                  </td>
+              </tr>
+            ))}
+            
           </tbody>
         </table>
 
